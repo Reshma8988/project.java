d3be08933b395b39304c469349152331c739f77c
Reshma8988 <147489050+Reshma8988@users.noreply.github.com>
2024-11-14 12:39:22 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,16 @@
+package swapping;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  a= 20;
+		int  b= 70;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a :" +a);
+		System.out.println("b :" +b);
+	}
+
+}
